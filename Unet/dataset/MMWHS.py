1509092167cb2091bde1e40453c7d146a82816c8
nhatmin2002 <134791579+nhatmin2002@users.noreply.github.com>
2024-08-19 17:00:09 +0700

    Update MMWHS.py

--- Unet/dataset/MMWHS.py
+++ Unet/dataset/MMWHS.py
@@ -478,9 +478,10 @@ def get_datasets_noPad(seed, on="train", fold_number=0, normalisation="minmax"):
 
 def get_datasets_Aug(seed, on="train", fold_number=0, normalisation="minmax",train_transforms = None ,val_transforms = None ):
     
-    patients_dir = sorted(glob("/home/fanxx/fxx/sdc/luoluo/MMWHS/MMWHS/ct_crop/" + "/ct_*_image.nii.gz"))
+    # patients_dir = sorted(glob("/home/fanxx/fxx/sdc/luoluo/MMWHS/MMWHS/ct_crop/" + "/ct_*_image.nii.gz"))
+    patients_dir = sorted(glob("/kaggle/input/thunghiemdata/data/ct_train/" + "/ct_*_image.nii.gz"))
 
-    kfold = KFold(5, shuffle=True, random_state=seed)
+    kfold = KFold(2, shuffle=True, random_state=seed)
     splits = list(kfold.split(patients_dir))
     train_idx, val_idx = splits[fold_number]
     len_val = len(val_idx)
